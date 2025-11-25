70665b73c6f63631445fbe7a8d035dbc66660ca7
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-11-25 09:51:11 +0200

    Enhance redirect configuration and update .gitignore for environment files

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const EXCLUDED_PATHS = ['/', '/contact-us', '/result'];
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'scarabio.com',
+          },
+        ],
+        missing: EXCLUDED_PATHS.map((path) => ({
+          type: 'path',
+          value: path,
+        })),
+        destination: 'https://blog.scarabio.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
