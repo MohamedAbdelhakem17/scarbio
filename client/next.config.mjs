ae7c406a6058782c1ce5e1204119e26246cbd727
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-11-25 10:30:49 +0200

    Remove redirect configuration from next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path((?!contact-us|result).+)',
-        has: [{ type: 'host', value: 'scarabio.com' }],
-        destination: 'https://blog.scarabio.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
