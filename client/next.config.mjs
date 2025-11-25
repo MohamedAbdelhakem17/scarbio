f833ff98be9c276478749b76c4ccfe439f2f3363
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-11-25 10:20:14 +0200

    edit config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const EXCLUDED_PATHS = ['/', '/contact', '/result'];
-
-const excludedRegex = new RegExp(
-  `^(${EXCLUDED_PATHS.map(path => path.replace(/\//g, '\\/')).join('|')})$`
-);
-
 const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/:path*',
-        has: [
-          { type: 'host', value: 'scarabio.com' },
-        ],
+        source: '/:path((?!contact-us|result).+)',
+        has: [{ type: 'host', value: 'scarabio.com' }],
+        destination: 'https://blog.scarabio.com/:path*',
         permanent: true,
-        destination: ({ params }) => {
-          const path = '/' + (params.path || '');
-          if (excludedRegex.test(path)) {
-            return null;
-          }
-          return `https://blog.scarabio.com/${params.path || ''}`;
-        },
       },
     ];
   },
