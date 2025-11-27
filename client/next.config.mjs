10d7fd37136154db9723774dacff6a56d8cb42e3
= <=>
2025-11-27 22:40:43 +0200

    edit

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,60 +2,60 @@
 
 const redirectsList = [
   {
-    from: '/%d8%af%d9%84%d9%8a%d9%84-%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%81%d9%8a-%d9%8a%d8%a7%d9%86%d8%af%d9%83%d8%b3/',
-    to: 'https://blog.scarabio.com/%d8%af%d9%84%d9%8a%d9%84-%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%81%d9%8a-%d9%8a%d8%a7%d9%86%d8%af%d9%83%d8%b3/',
+    from: '/دليل-تحسين-محركات-البحث-في-يانديكس/',
+    to: 'https://blog.scarabio.com/دليل-تحسين-محركات-البحث-في-يانديكس/',
   },
   {
-    from: '/%d8%a7%d8%ad%d8%b3%d9%86-%d9%85%d8%ad%d8%b1%d9%83-%d8%a8%d8%ad%d8%ab-%d9%84%d9%84%d8%b5%d9%88%d8%b1/',
-    to: 'https://blog.scarabio.com/%d8%a7%d8%ad%d8%b3%d9%86-%d9%85%d8%ad%d8%b1%d9%83-%d8%a8%d8%ad%d8%ab-%d9%84%d9%84%d8%b5%d9%88%d8%b1/',
+    from: '/احسن-محرك-بحث-للصور/',
+    to: 'https://blog.scarabio.com/احسن-محرك-بحث-للصور/',
   },
   {
-    from: '/%d8%a3%d9%81%d8%b6%d9%84-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a8%d8%ad%d8%ab-%d8%a7%d9%84%d9%81%d9%8a%d8%af%d9%8a%d9%88/',
-    to: 'https://blog.scarabio.com/%d8%a3%d9%81%d8%b6%d9%84-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a8%d8%ad%d8%ab-%d8%a7%d9%84%d9%81%d9%8a%d8%af%d9%8a%d9%88/',
+    from: '/أفضل-محركات-بحث-الفيديو/',
+    to: 'https://blog.scarabio.com/أفضل-محركات-بحث-الفيديو/',
   },
   {
-    from: '/%d9%85%d9%85%d9%8a%d8%b2%d8%a7%d8%aa-%d9%85%d8%ad%d8%b1%d9%83-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d8%a8%d9%8a%d9%86%d8%ac/',
-    to: 'https://blog.scarabio.com/%d9%85%d9%85%d9%8a%d8%b2%d8%a7%d8%aa-%d9%85%d8%ad%d8%b1%d9%83-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d8%a8%d9%8a%d9%86%d8%ac/',
+    from: '/مميزات-محرك-البحث-بينج/',
+    to: 'https://blog.scarabio.com/مميزات-محرك-البحث-بينج/',
   },
   {
-    from: '/%d9%85%d8%b9%d8%b1%d9%81%d8%a9-%d8%aa%d8%b1%d8%aa%d9%8a%d8%a8-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d9%81%d9%8a-%d8%ac%d9%88%d8%ac%d9%84/',
-    to: 'https://blog.scarabio.com/%d9%85%d8%b9%d8%b1%d9%81%d8%a9-%d8%aa%d8%b1%d8%aa%d9%8a%d8%a8-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d9%81%d9%8a-%d8%ac%d9%88%d8%ac%d9%84/',
+    from: '/معرفة-ترتيب-موقعك-في-جوجل/',
+    to: 'https://blog.scarabio.com/معرفة-ترتيب-موقعك-في-جوجل/',
   },
   {
-    from: '/%d8%b1%d9%81%d8%b9-%d8%aa%d8%b1%d8%aa%d9%8a%d8%a8-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d8%b9%d9%84%d9%89-%d8%ac%d9%88%d8%ac%d9%84/',
-    to: 'https://blog.scarabio.com/%d8%b1%d9%81%d8%b9-%d8%aa%d8%b1%d8%aa%d9%8a%d8%a8-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d8%b9%d9%84%d9%89-%d8%ac%d9%88%d8%ac%d9%84/',
+    from: '/رفع-ترتيب-موقعك-على-جوجل/',
+    to: 'https://blog.scarabio.com/رفع-ترتيب-موقعك-على-جوجل/',
   },
   {
-    from: '/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%81%d9%8a-%d8%a7%d9%84%d9%85%d9%88%d8%a7%d9%82%d8%b9-%d8%a7%d9%84%d8%ac%d8%af%d9%8a%d8%af%d8%a9/',
-    to: 'https://blog.scarabio.com/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%81%d9%8a-%d8%a7%d9%84%d9%85%d9%88%d8%a7%d9%82%d8%b9-%d8%a7%d9%84%d8%ac%d8%af%d9%8a%d8%af%d8%a9/',
+    from: '/تحسين-محركات-البحث-في-المواقع-الجديدة/',
+    to: 'https://blog.scarabio.com/تحسين-محركات-البحث-في-المواقع-الجديدة/',
   },
   {
-    from: '/%d9%84%d9%85%d8%a7%d8%b0%d8%a7-%d9%84%d8%a7-%d9%8a%d8%b8%d9%87%d8%b1-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d9%81%d9%8a-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab/',
-    to: 'https://blog.scarabio.com/%d9%84%d9%85%d8%a7%d8%b0%d8%a7-%d9%84%d8%a7-%d9%8a%d8%b8%d9%87%d8%b1-%d9%85%d9%88%d9%82%d8%b9%d9%83-%d9%81%d9%8a-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab/',
+    from: '/لماذا-لا-يظهر-موقعك-في-محركات-البحث/',
+    to: 'https://blog.scarabio.com/لماذا-لا-يظهر-موقعك-في-محركات-البحث/',
   },
   {
-    from: '/%d8%af%d9%84%d9%8a%d9%84-%d8%a8%d8%af%d8%a1-%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab/',
-    to: 'https://blog.scarabio.com/%d8%af%d9%84%d9%8a%d9%84-%d8%a8%d8%af%d8%a1-%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab/',
+    from: '/دليل-بدء-تحسين-محركات-البحث/',
+    to: 'https://blog.scarabio.com/دليل-بدء-تحسين-محركات-البحث/',
   },
   {
-    from: '/%d8%a7%d9%86%d9%88%d8%a7%d8%b9-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%88%d9%85%d9%85%d9%8a%d8%b2%d8%a7%d8%aa%d9%87%d8%a7/',
-    to: 'https://blog.scarabio.com/%d8%a7%d9%86%d9%88%d8%a7%d8%b9-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%88%d9%85%d9%85%d9%8a%d8%b2%d8%a7%d8%aa%d9%87%d8%a7/',
+    from: '/انواع-محركات-البحث-ومميزاتها/',
+    to: 'https://blog.scarabio.com/انواع-محركات-البحث-ومميزاتها/',
   },
   {
-    from: '/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d9%85%d8%b3%d8%aa%d8%b4%d9%81%d9%89-%d9%81%d9%8a-%d8%ac%d8%af%d8%a9/',
-    to: 'https://blog.scarabio.com/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d9%85%d8%b3%d8%aa%d8%b4%d9%81%d9%89-%d9%81%d9%8a-%d8%ac%d8%af%d8%a9/',
+    from: '/تحسين-محركات-بحث-لمستشفى-في-جدة/',
+    to: 'https://blog.scarabio.com/تحسين-محركات-بحث-لمستشفى-في-جدة/',
   },
   {
-    from: '/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d8%b4%d8%b1%d9%83%d8%a9-%d8%aa%d9%88%d8%b8%d9%8a%d9%81/',
-    to: 'https://blog.scarabio.com/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d8%b4%d8%b1%d9%83%d8%a9-%d8%aa%d9%88%d8%b8%d9%8a%d9%81/',
+    from: '/تحسين-محركات-بحث-لشركة-توظيف/',
+    to: 'https://blog.scarabio.com/تحسين-محركات-بحث-لشركة-توظيف/',
   },
   {
-    from: '/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d8%b4%d8%b1%d9%83%d8%a9-%d8%a7%d8%b3%d8%aa%d8%b4%d8%a7%d8%b1%d8%a7%d8%aa-%d9%85%d9%87%d9%86/',
-    to: 'https://blog.scarabio.com/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d8%b4%d8%b1%d9%83%d8%a9-%d8%a7%d8%b3%d8%aa%d8%b4%d8%a7%d8%b1%d8%a7%d8%aa-%d9%85%d9%87%d9%86/',
+    from: '/تحسين-محركات-بحث-لشركة-استشارات-مهن/',
+    to: 'https://blog.scarabio.com/تحسين-محركات-بحث-لشركة-استشارات-مهن/',
   },
   {
-    from: '/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d9%84%d8%b9%d8%b2%d9%84-%d8%a7%d9%84%d8%ad%d8%b1%d8%a7%d8%b1%d9%8a/',
-    to: 'https://blog.scarabio.com/%d8%aa%d8%ad%d8%b3%d9%8a%d9%86-%d9%85%d8%ad%d8%b1%d9%83%d8%a7%d8%aa-%d8%a7%d9%84%d8%a8%d8%ad%d8%ab-%d9%84%d9%84%d8%b9%d8%b2%d9%84-%d8%a7%d9%84%d8%ad%d8%b1%d8%a7%d8%b1%d9%8a/',
+    from: '/تحسين-محركات-البحث-للعزل-الحراري/',
+    to: 'https://blog.scarabio.com/تحسين-محركات-البحث-للعزل-الحراري/',
   },
 ];
 
